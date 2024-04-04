6150374fc95930d1c925f41d9b4196030bf3d7ee
Michael Craig <michaelcraigcoding@gmail.com>
2024-04-04 18:33:25 -0400

    Almost done with NumpyIn10

---
+++ ScikitLearnIn20/ScikitLearnIn20Personal.py
@@ -0,0 +1,23 @@
+# Setup a virtual environment using "python -m venv venv" in terminal, set venv to the working environment
+# Activate the virtual enviornment in terminal by using the command "venv/Scripts/activate", to deactivate, just type deactivate in the terminal
+# Install all dependencies for SciKitLearn using "pip install -U scikit-learn" in console
+# Install matplotlib and seaborn as well "pip install matplotlib seaborn"
+
+# Import necessary packages
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+''' ---Learning Pandas--- '''
+# Pandas is pretty much used everywhere to structure data in a way that Machine Learning Models can understand.
+# Pandas allows data to be organized into dataframes; think Excel Spreadsheets. Table/Tabular Data; Rows and Columns.
+# Pandas allows you to navigate and process through heaps of data faster and efficiently
+
+
+# Pandas can take in multiple file types and turn them into a dataframe
+
+''' ---Learning Matplotlib--- '''
+
+
+''' --- Learning Seaborn --- '''
+
